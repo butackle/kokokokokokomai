85cad88f799a0261d8de1be0d268edb5574b2ba7
puru <butackle@gmail.com>
2025-05-31 18:13:18 +0900

    Add Japanese era (wareki) to Gregorian year mapping in wareki_seireki.js

---
+++ js/wareki_seireki.js
@@ -0,0 +1,702 @@
+const warekiSeireki = [
+	{
+		wareki: "明治1年",
+		seireki: 1868
+	},
+	{
+		wareki: "明治2年",
+		seireki: 1869
+	},
+	{
+		wareki: "明治3年",
+		seireki: 1870
+	},
+	{
+		wareki: "明治4年",
+		seireki: 1871
+	},
+	{
+		wareki: "明治5年",
+		seireki: 1872
+	},
+	{
+		wareki: "明治6年",
+		seireki: 1873
+	},
+	{
+		wareki: "明治7年",
+		seireki: 1874
+	},
+	{
+		wareki: "明治8年",
+		seireki: 1875
+	},
+	{
+		wareki: "明治9年",
+		seireki: 1876
+	},
+	{
+		wareki: "明治10年",
+		seireki: 1877
+	},
+	{
+		wareki: "明治11年",
+		seireki: 1878
+	},
+	{
+		wareki: "明治12年",
+		seireki: 1879
+	},
+	{
+		wareki: "明治13年",
+		seireki: 1880
+	},
+	{
+		wareki: "明治14年",
+		seireki: 1881
+	},
+	{
+		wareki: "明治15年",
+		seireki: 1882
+	},
+	{
+		wareki: "明治16年",
+		seireki: 1883
+	},
+	{
+		wareki: "明治17年",
+		seireki: 1884
+	},
+	{
+		wareki: "明治18年",
+		seireki: 1885
+	},
+	{
+		wareki: "明治19年",
+		seireki: 1886
+	},
+	{
+		wareki: "明治20年",
+		seireki: 1887
+	},
+	{
+		wareki: "明治21年",
+		seireki: 1888
+	},
+	{
+		wareki: "明治22年",
+		seireki: 1889
+	},
+	{
+		wareki: "明治23年",
+		seireki: 1890
+	},
+	{
+		wareki: "明治24年",
+		seireki: 1891
+	},
+	{
+		wareki: "明治25年",
+		seireki: 1892
+	},
+	{
+		wareki: "明治26年",
+		seireki: 1893
+	},
+	{
+		wareki: "明治27年",
+		seireki: 1894
+	},
+	{
+		wareki: "明治28年",
+		seireki: 1895
+	},
+	{
+		wareki: "明治29年",
+		seireki: 1896
+	},
+	{
+		wareki: "明治30年",
+		seireki: 1897
+	},
+	{
+		wareki: "明治31年",
+		seireki: 1898
+	},
+	{
+		wareki: "明治32年",
+		seireki: 1899
+	},
+	{
+		wareki: "明治33年",
+		seireki: 1900
+	},
+	{
+		wareki: "明治34年",
+		seireki: 1901
+	},
+	{
+		wareki: "明治35年",
+		seireki: 1902
+	},
+	{
+		wareki: "明治36年",
+		seireki: 1903
+	},
+	{
+		wareki: "明治37年",
+		seireki: 1904
+	},
+	{
+		wareki: "明治38年",
+		seireki: 1905
+	},
+	{
+		wareki: "明治39年",
+		seireki: 1906
+	},
+	{
+		wareki: "明治40年",
+		seireki: 1907
+	},
+	{
+		wareki: "明治41年",
+		seireki: 1908
+	},
+	{
+		wareki: "明治42年",
+		seireki: 1909
+	},
+	{
+		wareki: "明治43年",
+		seireki: 1910
+	},
+	{
+		wareki: "明治44年",
+		seireki: 1911
+	},
+	{
+		wareki: "明治45年",
+		seireki: 1912
+	},
+	{
+		wareki: "大正1年",
+		seireki: 1912
+	},
+	{
+		wareki: "大正2年",
+		seireki: 1913
+	},
+	{
+		wareki: "大正3年",
+		seireki: 1914
+	},
+	{
+		wareki: "大正4年",
+		seireki: 1915
+	},
+	{
+		wareki: "大正5年",
+		seireki: 1916
+	},
+	{
+		wareki: "大正6年",
+		seireki: 1917
+	},
+	{
+		wareki: "大正7年",
+		seireki: 1918
+	},
+	{
+		wareki: "大正8年",
+		seireki: 1919
+	},
+	{
+		wareki: "大正9年",
+		seireki: 1920
+	},
+	{
+		wareki: "大正10年",
+		seireki: 1921
+	},
+	{
+		wareki: "大正11年",
+		seireki: 1922
+	},
+	{
+		wareki: "大正12年",
+		seireki: 1923
+	},
+	{
+		wareki: "大正13年",
+		seireki: 1924
+	},
+	{
+		wareki: "大正14年",
+		seireki: 1925
+	},
+	{
+		wareki: "大正15年",
+		seireki: 1926
+	},
+	{
+		wareki: "昭和1年",
+		seireki: 1926
+	},
+	{
+		wareki: "昭和2年",
+		seireki: 1927
+	},
+	{
+		wareki: "昭和3年",
+		seireki: 1928
+	},
+	{
+		wareki: "昭和4年",
+		seireki: 1929
+	},
+	{
+		wareki: "昭和5年",
+		seireki: 1930
+	},
+	{
+		wareki: "昭和6年",
+		seireki: 1931
+	},
+	{
+		wareki: "昭和7年",
+		seireki: 1932
+	},
+	{
+		wareki: "昭和8年",
+		seireki: 1933
+	},
+	{
+		wareki: "昭和9年",
+		seireki: 1934
+	},
+	{
+		wareki: "昭和10年",
+		seireki: 1935
+	},
+	{
+		wareki: "昭和11年",
+		seireki: 1936
+	},
+	{
+		wareki: "昭和12年",
+		seireki: 1937
+	},
+	{
+		wareki: "昭和13年",
+		seireki: 1938
+	},
+	{
+		wareki: "昭和14年",
+		seireki: 1939
+	},
+	{
+		wareki: "昭和15年",
+		seireki: 1940
+	},
+	{
+		wareki: "昭和16年",
+		seireki: 1941
+	},
+	{
+		wareki: "昭和17年",
+		seireki: 1942
+	},
+	{
+		wareki: "昭和18年",
+		seireki: 1943
+	},
+	{
+		wareki: "昭和19年",
+		seireki: 1944
+	},
+	{
+		wareki: "昭和20年",
+		seireki: 1945
+	},
+	{
+		wareki: "昭和21年",
+		seireki: 1946
+	},
+	{
+		wareki: "昭和22年",
+		seireki: 1947
+	},
+	{
+		wareki: "昭和23年",
+		seireki: 1948
+	},
+	{
+		wareki: "昭和24年",
+		seireki: 1949
+	},
+	{
+		wareki: "昭和25年",
+		seireki: 1950
+	},
+	{
+		wareki: "昭和26年",
+		seireki: 1951
+	},
+	{
+		wareki: "昭和27年",
+		seireki: 1952
+	},
+	{
+		wareki: "昭和28年",
+		seireki: 1953
+	},
+	{
+		wareki: "昭和29年",
+		seireki: 1954
+	},
+	{
+		wareki: "昭和30年",
+		seireki: 1955
+	},
+	{
+		wareki: "昭和31年",
+		seireki: 1956
+	},
+	{
+		wareki: "昭和32年",
+		seireki: 1957
+	},
+	{
+		wareki: "昭和33年",
+		seireki: 1958
+	},
+	{
+		wareki: "昭和34年",
+		seireki: 1959
+	},
+	{
+		wareki: "昭和35年",
+		seireki: 1960
+	},
+	{
+		wareki: "昭和36年",
+		seireki: 1961
+	},
+	{
+		wareki: "昭和37年",
+		seireki: 1962
+	},
+	{
+		wareki: "昭和38年",
+		seireki: 1963
+	},
+	{
+		wareki: "昭和39年",
+		seireki: 1964
+	},
+	{
+		wareki: "昭和40年",
+		seireki: 1965
+	},
+	{
+		wareki: "昭和41年",
+		seireki: 1966
+	},
+	{
+		wareki: "昭和42年",
+		seireki: 1967
+	},
+	{
+		wareki: "昭和43年",
+		seireki: 1968
+	},
+	{
+		wareki: "昭和44年",
+		seireki: 1969
+	},
+	{
+		wareki: "昭和45年",
+		seireki: 1970
+	},
+	{
+		wareki: "昭和46年",
+		seireki: 1971
+	},
+	{
+		wareki: "昭和47年",
+		seireki: 1972
+	},
+	{
+		wareki: "昭和48年",
+		seireki: 1973
+	},
+	{
+		wareki: "昭和49年",
+		seireki: 1974
+	},
+	{
+		wareki: "昭和50年",
+		seireki: 1975
+	},
+	{
+		wareki: "昭和51年",
+		seireki: 1976
+	},
+	{
+		wareki: "昭和52年",
+		seireki: 1977
+	},
+	{
+		wareki: "昭和53年",
+		seireki: 1978
+	},
+	{
+		wareki: "昭和54年",
+		seireki: 1979
+	},
+	{
+		wareki: "昭和55年",
+		seireki: 1980
+	},
+	{
+		wareki: "昭和56年",
+		seireki: 1981
+	},
+	{
+		wareki: "昭和57年",
+		seireki: 1982
+	},
+	{
+		wareki: "昭和58年",
+		seireki: 1983
+	},
+	{
+		wareki: "昭和59年",
+		seireki: 1984
+	},
+	{
+		wareki: "昭和60年",
+		seireki: 1985
+	},
+	{
+		wareki: "昭和61年",
+		seireki: 1986
+	},
+	{
+		wareki: "昭和62年",
+		seireki: 1987
+	},
+	{
+		wareki: "昭和63年",
+		seireki: 1988
+	},
+	{
+		wareki: "昭和64年",
+		seireki: 1989
+	},
+	{
+		wareki: "平成1年",
+		seireki: 1989
+	},
+	{
+		wareki: "平成2年",
+		seireki: 1990
+	},
+	{
+		wareki: "平成3年",
+		seireki: 1991
+	},
+	{
+		wareki: "平成4年",
+		seireki: 1992
+	},
+	{
+		wareki: "平成5年",
+		seireki: 1993
+	},
+	{
+		wareki: "平成6年",
+		seireki: 1994
+	},
+	{
+		wareki: "平成7年",
+		seireki: 1995
+	},
+	{
+		wareki: "平成8年",
+		seireki: 1996
+	},
+	{
+		wareki: "平成9年",
+		seireki: 1997
+	},
+	{
+		wareki: "平成10年",
+		seireki: 1998
+	},
+	{
+		wareki: "平成11年",
+		seireki: 1999
+	},
+	{
+		wareki: "平成12年",
+		seireki: 2000
+	},
+	{
+		wareki: "平成13年",
+		seireki: 2001
+	},
+	{
+		wareki: "平成14年",
+		seireki: 2002
+	},
+	{
+		wareki: "平成15年",
+		seireki: 2003
+	},
+	{
+		wareki: "平成16年",
+		seireki: 2004
+	},
+	{
+		wareki: "平成17年",
+		seireki: 2005
+	},
+	{
+		wareki: "平成18年",
+		seireki: 2006
+	},
+	{
+		wareki: "平成19年",
+		seireki: 2007
+	},
+	{
+		wareki: "平成20年",
+		seireki: 2008
+	},
+	{
+		wareki: "平成21年",
+		seireki: 2009
+	},
+	{
+		wareki: "平成22年",
+		seireki: 2010
+	},
+	{
+		wareki: "平成23年",
+		seireki: 2011
+	},
+	{
+		wareki: "平成24年",
+		seireki: 2012
+	},
+	{
+		wareki: "平成25年",
+		seireki: 2013
+	},
+	{
+		wareki: "平成26年",
+		seireki: 2014
+	},
+	{
+		wareki: "平成27年",
+		seireki: 2015
+	},
+	{
+		wareki: "平成28年",
+		seireki: 2016
+	},
+	{
+		wareki: "平成29年",
+		seireki: 2017
+	},
+	{
+		wareki: "平成30年",
+		seireki: 2018
+	},
+	{
+		wareki: "平成31年",
+		seireki: 2019
+	},
+	{
+		wareki: "令和1年",
+		seireki: 2019
+	},
+	{
+		wareki: "令和2年",
+		seireki: 2020
+	},
+	{
+		wareki: "令和3年",
+		seireki: 2021
+	},
+	{
+		wareki: "令和4年",
+		seireki: 2022
+	},
+	{
+		wareki: "令和5年",
+		seireki: 2023
+	},
+	{
+		wareki: "令和6年",
+		seireki: 2024
+	},
+	{
+		wareki: "令和7年",
+		seireki: 2025
+	},
+	{
+		wareki: "令和8年",
+		seireki: 2026
+	},
+	{
+		wareki: "令和9年",
+		seireki: 2027
+	},
+	{
+		wareki: "令和10年",
+		seireki: 2028
+	},
+	{
+		wareki: "令和11年",
+		seireki: 2029
+	},
+	{
+		wareki: "令和12年",
+		seireki: 2030
+	},
+	{
+		wareki: "令和13年",
+		seireki: 2031
+	},
+	{
+		wareki: "令和14年",
+		seireki: 2032
+	},
+	{
+		wareki: "令和15年",
+		seireki: 2033
+	},
+	{
+		wareki: "令和16年",
+		seireki: 2034
+	},
+	{
+		wareki: "令和17年",
+		seireki: 2035
+	},
+	{
+		wareki: "令和18年",
+		seireki: 2036
+	},
+	{
+		wareki: "令和19年",
+		seireki: 2037
+	},
+	{
+		wareki: "令和20年",
+		seireki: 2038
+	}
+]
