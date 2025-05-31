4d33a486bf008e06fc7255a43832fb725d942d91
puru <butackle@gmail.com>
2025-05-31 17:35:51 +0900

    Add question selection and game mode functionality in index.html and index.js

--- js/_common.js
+++ js/_common.js
@@ -1,1 +1,7 @@
+const INDEX_PAGE_URL = new URL("index.html", location.href);
+const GAME_PAGE_URL = new URL("game.html", location.href);
+const QUESTION_PARAM = "q";
+const MODE_PARAM = "m";
+const WAREKI_MODE = "wareki";
+
 const currentYear = new Date().getFullYear();
--- js/index.js
+++ js/index.js
@@ -1,7 +1,38 @@
+// ゲームモード選択
+const selectQuestion = document.getElementById('questions');
+const radioMode = document.querySelectorAll('input[name="mode"]');
+const gameStartLink = document.getElementById('gameStartLink');
+const setGameLinkParams = (paramName, paramValue) => {
+	const linkHref = gameStartLink.getAttribute('href');
+	const gamePageURL = linkHref ? new URL(linkHref) : GAME_PAGE_URL;
+	const gamePageParams = gamePageURL.searchParams;
+	gamePageParams.set(paramName, paramValue);
+	gameStartLink.href = gamePageURL;
+}
+const setQuestion = () => {
+	const selectedValue = selectQuestion.value;
+	setGameLinkParams(QUESTION_PARAM, selectedValue);
+}
+radioMode.forEach((radio) =>
+	radio.addEventListener('change', () => {
+		radio.checked && setGameLinkParams(MODE_PARAM, radio.value);
+	})
+);
+selectQuestion.addEventListener('change', setQuestion);
+document.addEventListener('DOMContentLoaded', () => {
+	setQuestion();
+	const warekiRadioBtn = document.getElementById('wareki_radio');
+	if (warekiRadioBtn) {
+		warekiRadioBtn.value = WAREKI_MODE;
+	}
+});
+
+
 // 古米の定義ボタンを押された時の動作
 const defineDialog = document.getElementById('definitionDialog');
 const definitionOpenBtn = document.getElementById('definitionOpenBtn');
-const definitionCloseBtn = document.getElementById('definitionCloseBtn');definitionOpenBtn.addEventListener('click', () => defineDialog.showModal());
+const definitionCloseBtn = document.getElementById('definitionCloseBtn');
+definitionOpenBtn.addEventListener('click', () => defineDialog.showModal());
 definitionCloseBtn.addEventListener('click', () => defineDialog.close());
 
 // 前年・前々年を入れる処理
